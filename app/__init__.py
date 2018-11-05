bf6c77bc971440d0880421f8c84c1c783921b4bc
Raul Rios <raulrios1@live.com>
2018-11-05 09:13:25 -0600

    Removed unnecessary module import

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
 import os
-from flask_images import Images
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
-images = Images(app)
 app.config.update(
     # UPLOADED_PATH=os.path.join(basedir, 'uploads'),
     # Flask-Dropzone config:
