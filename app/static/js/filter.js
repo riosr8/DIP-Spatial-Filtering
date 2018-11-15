0e47140113b3eac86e35c679ad400fddb220177d
Raul Rios <raulrios1@live.com>
2018-11-15 14:41:34 -0600

    initial attempt for applying a filter on page load

--- app/static/js/filter.js
+++ app/static/js/filter.js
@@ -1,18 +1,33 @@
-
-$(function() {
-    $('button').click(function() {
+$(function () {
+    $('button').click(function () {
         $.ajax({
             url: '/processImage',
             data: $('form').serialize(),
             type: 'POST',
-            success: function(response) {
+            success: function (response) {
                 // console.log(response);
 
                 $('#filtered_img_res').attr('src', `data:image/jpg;base64,${response}`);
             },
-            error: function(error) {
+            error: function (error) {
                 console.log(error);
             }
         });
+    });
+});
+
+$(document).ready(function () {
+    $.ajax({
+        url: '/processImage',
+        data: $('form').serialize(),
+        type: 'POST',
+        success: function (response) {
+            // console.log(response);
+
+            $('#filtered_img_res').attr('src', `data:image/jpg;base64,${response}`);
+        },
+        error: function (error) {
+            console.log(error);
+        }
     });
 });
