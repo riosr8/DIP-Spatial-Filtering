81205e05358f29095d15b0d7e383411976b3e138
Raul Rios <raulrios1@live.com>
2018-11-25 00:51:32 -0600

    update ui with currently applied filter and parameters

--- app/static/js/filter.js
+++ app/static/js/filter.js
@@ -5,8 +5,11 @@ $(function () {
             data: $('form').serialize(),
             type: 'POST',
             success: function (response) {
-                // console.log(response);
-
+                console.log(response);
+                $('#current_filter').text('Currently selected filter: ' +  $( "select#filters option:checked" ).val());
+                $('#current_mask').text('Mask size: ' + $('#mask_size').val() + 'x' + $('#mask_size').val());
+                $('#current_kvalue').text('k-Value: ' + $('#k_value').val());
+                $('#current_threshold').text('threshold: ' + $('#threshold').val());
                 $('#filtered_img_res').attr('src', `data:image/jpg;base64,${response}`);
             },
             error: function (error) {
@@ -16,18 +19,18 @@ $(function () {
     });
 });
 
-$(document).ready(function () {
-    $.ajax({
-        url: '/processImage',
-        data: $('form').serialize(),
-        type: 'POST',
-        success: function (response) {
-            // console.log(response);
+// $(document).ready(function () {
+//     $.ajax({
+//         url: '/processImage',
+//         data: $('form').serialize(),
+//         type: 'POST',
+//         success: function (response) {
+//             // console.log(response);
 
-            $('#filtered_img_res').attr('src', `data:image/jpg;base64,${response}`);
-        },
-        error: function (error) {
-            console.log(error);
-        }
-    });
-});
+//             $('#filtered_img_res').attr('src', `data:image/jpg;base64,${response}`);
+//         },
+//         error: function (error) {
+//             console.log(error);
+//         }
+//     });
+// });
