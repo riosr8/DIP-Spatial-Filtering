b0b56e386bb60077350ffe1e112bf2057fc8544e
Raul Rios <raulrios1@live.com>
2018-12-06 15:52:32 -0600

    renamed input_image -> image

--- app/unsharp_mask.py
+++ app/unsharp_mask.py
@@ -10,17 +10,17 @@ def unsharp_mask(image, filter_size, k, threshold=0):
 
     # Unsharp mask filter
     # smooth_image = cv2.GaussianBlur(image,(filter_size,filter_size),0)  #testing purposes
-    smooth_image = gaussian_yourchoice(input_image, filter_size)
-    image_difference = cv2.absdiff(input_image, smooth_image)
+    smooth_image = gaussian_yourchoice(image, filter_size)
+    image_difference = cv2.absdiff(image, smooth_image)
     actual_mask = k * image_difference
     if(threshold == 0):
-        final_image = cv2.add(input_image, actual_mask)
+        final_image = cv2.add(image, actual_mask)
     else:
         for i in range(m):
             for j in range(n):
                 if(image_difference[i][j] > threshold):
-                    final_image[i][j] = input_image[i][j] + actual_mask[i][j]
+                    final_image[i][j] = image[i][j] + actual_mask[i][j]
                 else:
-                    final_image[i][j] = input_image[i][j]
+                    final_image[i][j] = image[i][j]
 
     return final_image
