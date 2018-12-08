b2d2ae283b8725d8b854875dcdd263b7a002e956
ayupriya <aria.m0996@gmail.com>
2018-12-07 20:27:27 -0600

    Update sharp_image.py

--- app/sharp_image.py
+++ app/sharp_image.py
@@ -38,10 +38,12 @@ def neg_nonzero(image, filter_size):
 
 
 def convolution(image, mask):
-    w, h = np.shape(image)
+    w = image.shape[0]
+    h = image.shape[1]
+
     flip_mask = np.flipud(np.fliplr(mask))
     w1, h1 = np.shape(flip_mask)
-    image_pad = np.zeros((image.shape[0] + 2, image.shape[1] + 2))
+    image_pad = np.zeros((w + 2, h + 2))
     image_pad[1:-1, 1:-1] = image
 
     new_img = np.zeros((w, h))
